0b30a99551da09de2e6130b00e0e4de08ce62afb
github-hsh <2104179425@qq.com>
2019-10-19 10:17:10 +0800

    第二次任务

---
+++ rober.cpp
@@ -0,0 +1,35 @@
+#include<QApplication>
+#include<iostream>
+
+
+
+#include"mywidget"
+int main(int argc,chat *argv[])
+{
+
+
+
+
+        QApplication app(argc,argv);
+
+        QWidget a;
+        a.show();
+        return app.exec();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
