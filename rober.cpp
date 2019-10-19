f130804a1e250d3c22c1ce15a94a473a0b4e1a70
github-hsh <2104179425@qq.com>
2019-10-19 11:07:20 +0800

    edit by hot_fix NO.3

--- rober.cpp
+++ rober.cpp
@@ -5,7 +5,7 @@ int main(int argc,chat *argv[])
         QApplication app(argc,argv);
         QWidget a;
 	a.setWindowTitle("huashanjianfa");
-        a.show()
+        a.show()  //如果没有，无法弹出窗口 
 	exec();
         return 0; 
 }
